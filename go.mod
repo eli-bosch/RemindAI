92ca9a00e6dc86344a330681c38c62fbacd53132
Eli Bosch <eli.bosch04@gmail.com>
2025-04-14 10:30:42 -0500

    Added models

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/eli-bosch/remindAI
 
 go 1.24.1
 
-require github.com/gorilla/mux v1.8.1 // indirect
+require (
+	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/jinzhu/gorm v1.9.16 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+)
---
+++ internal/models/models.go
@@ -0,0 +1,39 @@
+package models
+
+import (
+	"time"
+
+	_ "github.com/jinzhu/gorm"
+)
+
+type User struct {
+	ID        uint       `gorm:"primaryKey" json:"user_id"`
+	Username  string     `gorm:"column:username;size:32;not null; unique" json:"username"`
+	Password  string     `gorm:"column:password;size:128;not null" json:"password"`
+	Phone     string     `gorm:"column:password;size:15" json:"phone"`
+	First     string     `gorm:"column:first;size:50" json:"first"`
+	Last      string     `gorm:"column:last;size:50" json:"last"`
+	Address   string     `gorm:"column:address;size:100" json:"address"`
+	City      string     `gorm:"column:city;size:50" json:"city"`
+	Country   string     `gorm:"column:country;size:50" json:"country"`
+	Reminders []Reminder `gorm:"constraint:OnDelete:CASCADE"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+type Reminder struct {
+	ID          uint      `gorm:"primaryKey" json:"primaryKey"`
+	UserID      uint      `gorm:"not null;index" json:"user_id"`
+	Title       string    `gorm:"column:title;size 100;not null" json:"title"`
+	Description string    `gorm:"column:description;size:255" json:"description"`
+	Address     string    `gorm:"column:address;size:100" json:"address"`
+	City        string    `gorm:"column:city;size:50" json:"city"`
+	Zip         string    `gorm:"column:zip;size:10" json:"zip"`
+	Country     string    `gorm:"column:country;size:50" json:"country"`
+	Time        time.Time `gorm:"column:time;not null" json:"time"`
+	EarlyRemind int8      `gorm:"column:early_remind" json:"early_remind"`
+	Repeating   bool      `gorm:"column:repeating" json:"repeating"`
+	EndDate     time.Time `gorm:"column:end_date" json:"end_date"`
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+}
